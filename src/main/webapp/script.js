78260c74f2c246908eec5a4eec7b85591d24b3e1
augusi <august.indal@stud.ntnu.no>
2017-09-23 19:55:55 +0200

    Added put

---
+++ src/main/webapp/script.js
@@ -0,0 +1,51 @@
+/**
+ * Created by August on 23.09.2017.
+ */
+$(document).ready(function() {
+
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+    })
+
+    setInterval(function () {
+            $('#myTable').DataTable().ajax.reload();
+        },
+        1000);
+    $('#myTable').DataTable( {
+        ajax: {
+            url: 'rest/quiz/',
+            dataSrc: '',
+            dataType: "json"
+        },
+        columns: [
+            { data: 'qid' },
+            { data: 'name' }
+        ]
+    });
+
+    $("#createQuiz").click(function () {
+        $.ajax({
+            url: 'rest/quiz/',
+            type: 'POST',
+            data: JSON.stringify({
+                qid: $("#quizId").val(),
+                name: $("#quizName").val(),
+                questions: [
+                    {
+                        questionId : $("#questionId").val(),
+                        question : $("#question").val(),
+                        sec : $("#sec").val(),
+                        opt :[],
+                        svarInd : ($("#rikigsvar").val() - 1)
+                    }
+                ]
+            }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (result) {
+                $('#myTable').DataTable().ajax.reload();
+            }
+        });
+    });
+
+});
